db90ded9d843083e45b873dc4df428e9bbbb5302
Andrei Joda <37514267+andreijoda@users.noreply.github.com>
2021-10-07 22:57:22 -0300

    Tirado cliente 10

--- app.js
+++ app.js
@@ -324,32 +324,7 @@ client9.on('join', (channel, username, self) => {
     }
 });
 
-//cliente 10 Grv
-const client10 = new tmi.Client({
-	options: { debug: false, messagesLogLevel: "warn" },
-	connection: {
-		reconnect: true,
-		secure: true
-	},
-	identity: {
-		username: 'LeandroBrito_',
-		password: 'oauth:u7vift2fnjolvxy1hej4znpi7kb6cz'
-	},
-	channels: canaistofarm
-});
-
-client10.connect();
-client10.on('message', (channel, tags, message, self) => {
-	if(self) return;
-	if(message.toLowerCase() === '!dreifarm') {
-		client10.say(channel, `@${tags.username}, Farm em Andamento!`);
-	}
-});
-client10.on('join', (channel, username, self) => {
-    if(self) {
-        client10.log.error(`Joined10 ${channel}`);
-    }
-});
+// CLIENTE 10 FALTA FALTA FALTA
 
 //cliente 11 Rafa
 const client11 = new tmi.Client({
